344b9ed18177a26698ef9ef1482c20b980c9b146
Mi13285 <mixkud95@inbox.ru>
2024-12-31 10:05:01 +0300

    first commit

--- script.js
+++ script.js
@@ -2,4 +2,3 @@ function flattenAndSort(array) {
   return array.flat().sort((a, b) => a - b);
 }
 console.log(flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]));
-console.log(flattenAndSort([[], [1]]), [1]);
